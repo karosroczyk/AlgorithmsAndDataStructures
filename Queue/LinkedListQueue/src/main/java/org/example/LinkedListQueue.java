d22c0d8942a95364e3950cfcfe6c029125cc9de9
karosroczyk <50304244+karosroczyk@users.noreply.github.com>
2024-02-19 08:37:16 +0100

    LinkedListQueue test still implementing

---
+++ Queue/LinkedListQueue/src/main/java/org/example/LinkedListQueue.java
@@ -0,0 +1,196 @@
+package org.example;
+
+import java.util.NoSuchElementException;
+public class LinkedListQueue<E> {
+    private static class Element<E> {
+        Element<E> next, prev;
+        E value;
+
+        public Element(Element<E> next, Element<E> prev, E value){
+            this.next = next;
+            this.prev = prev;
+            this.value = value;
+        }
+    }
+    Element<E> head, tail;
+    int size;
+    /**
+     * O(1)
+     * @return The number of elements in the list.
+     */
+    public int size(){
+        return size;
+    }
+    /** Gets the first element.
+     * @return The head of the list.
+     * @throws NoSuchElementException if the list is empty.
+     * Time complexity: O(1)
+     */
+    public E getFirst() {
+        if(isEmpty()) throw new NoSuchElementException();
+        return head.value;
+    }
+    /**
+     * Inserts the given element at the beginning of this list.
+     *    // O(1)
+     *    //linkFirst
+     * @param value An element to insert into the list.
+     */
+    public void addFirst(E value){
+        Element<E> head_tmp = head;
+        Element<E> element = new Element<E>(head, null, value);
+
+
+        if(isEmpty()) {
+            tail = element;
+        }else{
+            head_tmp.prev = element;
+        }
+        head = element;
+        size++;
+    }
+    /**
+     * Removes the first element from the list.
+     *    // O(1)
+     *    //unlinkFirst
+     * @throws NoSuchElementException if the list is empty.
+     */
+    public void removeFirst(){
+        if(isEmpty()) throw new NoSuchElementException();
+        size--;
+        if(head.next == null && tail.next == null) {
+            clear();
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+    }
+    /** Gets the last element.
+     * @return The tail of the list.
+     * @throws NoSuchElementException if the list is empty.
+     * Time complexity: O(1)
+     */
+    public E getLast() {
+        if(isEmpty()) throw new NoSuchElementException();
+        return tail.value;
+    }
+    /**
+     * Inserts the given element at the end of this list.
+     *    // O(1)
+     *    //linkLast
+     * @param value An element to insert into the list.
+     */
+    public void addLast(E value){
+        Element<E> tail_tmp = tail;
+        Element<E> element = new Element<E>(null, tail, value);
+
+        if(isEmpty()) {
+            head = element;
+        }else{
+            tail_tmp.next = element;
+        }
+
+        tail = element;
+        size++;
+    }
+    /**
+     * Removes the first element from the list.
+     *    // O(1)
+     *    //unlinkLast
+     * @throws NoSuchElementException if the list is empty.
+     */
+    public void removeLast(){
+        if(isEmpty()) throw new NoSuchElementException();
+        size--;
+        if (head.next == null && head.prev == null){
+            clear();
+            return;
+        }
+        tail = tail.prev;
+        tail.next = null;
+    }
+
+    //--------------------Queue Interface Implementation--------------
+    public E element() throws NoSuchElementException { return getFirst(); }
+    public E peek(){ return getFirst(); }
+    public boolean add(E element){
+        addLast(element);
+        return true;
+    }
+    public boolean offer(E element){
+        addLast(element);
+        return true;
+    }
+    public E remove(){
+        E tmp = getFirst();
+        removeFirst();
+        return tmp;
+    }
+    public E poll(){
+        E tmp = getFirst();
+        removeFirst();
+        return tmp;
+    }
+    //----------------------------------------------------------------
+    /**
+     * Returns element at given index.
+     *    // O(n/2) ??
+     * @param index Index of element which should be returned.
+     * @throws NoSuchElementException if the list is empty.
+     * @throws IndexOutOfBoundsException if index is out of range (0, size()).
+     */
+    public E get(int index){
+        if(isEmpty()) throw new NoSuchElementException();
+        if(index < 0 || index >= size())
+            throw new IndexOutOfBoundsException();
+
+        Element<E> head_tmp = head;
+        Element<E> tail_tmp = tail;
+        int count = size()/2;
+        if(index <= count){
+            while(index-- > 0){
+                head_tmp = head_tmp.next;
+            }
+            return head_tmp.value;
+        }else{
+            while(index-- > count){
+                tail_tmp = tail_tmp.prev;
+            }
+            return tail_tmp.value;
+        }
+
+
+    }
+    /**
+     * Removes all elements of list.
+     * O(1)
+     */
+    public void clear(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    /**
+     * Check if list is empty.
+     * O(1)
+     * @return Boolean depending on whether list is empty.
+     */
+    public boolean isEmpty(){
+        return (head == null && tail == null);
+    }
+    /**
+     * Iterate over list.
+     * O(n)
+     * @return String in format [elem1, elem2, ..., elemn].
+     */
+    public String iterate(){
+        Element<E> head_tmp = head;
+        String output = "[";
+        while(head_tmp != null){
+            output += head_tmp.value + ", ";
+            head_tmp = head_tmp.next;
+        }
+        output = output.substring(0, output.length() - 2);
+        return output + "]";
+    }
+}
--- Queue/LinkedListQueue/src/test/java/MyLinkedList/LinkedListQueueTest.java
+++ Queue/LinkedListQueue/src/test/java/MyLinkedList/LinkedListQueueTest.java
@@ -1,2 +1,294 @@
-package MyLinkedList;public class MyLinkedListTest {
+package LinkedListQueue;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.example.LinkedListQueue;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class LinkedListQueueTest {
+    /**
+     * Tests for size()
+     */
+    @Test
+    public void sizeIsZeroWhenListIsEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertEquals(0, LinkedListQueue.size());
+    }
+    /**
+     * Tests for addFirst()
+     */
+    @ParameterizedTest
+    @MethodSource("LinkedListQueueData")
+    public void addFirstIncrementSize(Integer... dataToAdd){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        int size = 0;
+        for(int i = 0; i < dataToAdd.length; i++){
+            LinkedListQueue.addFirst(dataToAdd[i]);
+            assertEquals(size + (i + 1), LinkedListQueue.size());
+        }
+    }
+    /**
+     * Tests for addLast()
+     */
+    @ParameterizedTest
+    @MethodSource("LinkedListQueueData")
+    public void addLastIncrementSize(Integer... dataToAdd){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        int size = 0;
+        for(int i = 0; i < dataToAdd.length; i++){
+            LinkedListQueue.addLast(dataToAdd[i]);
+            assertEquals(size + (i + 1), LinkedListQueue.size());
+        }
+    }
+    /**
+     * Tests for removeFirst()
+     */
+    @Test
+    public void removeFirstThrowsExceptionWhenEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertThrows(NoSuchElementException.class,
+                LinkedListQueue::removeFirst);
+    }
+    @Test
+    public void removeFirstDecrementSize(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(15);
+        LinkedListQueue.addLast(25);
+        int sizeBeforeRemove = LinkedListQueue.size();
+        for(int i = 0; i < sizeBeforeRemove; i++)
+        {
+            LinkedListQueue.removeFirst();
+            assertEquals(sizeBeforeRemove - (i + 1), LinkedListQueue.size());
+        }
+    }
+    /**
+     * Tests for removeLast()
+     */
+    @Test
+    public void removeLastThrowsExceptionWhenEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertThrows(NoSuchElementException.class,
+                LinkedListQueue::removeLast);
+    }
+    @Test
+    public void removeLastDecrementSize(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(15);
+        LinkedListQueue.addLast(25);
+        int sizeBeforeRemove = LinkedListQueue.size();
+        for(int i = 0; i < sizeBeforeRemove; i++)
+        {
+            LinkedListQueue.removeLast();
+            assertEquals(sizeBeforeRemove - (i + 1), LinkedListQueue.size());
+        }
+    }
+    /**
+     * Tests for getFirst()
+     */
+    @Test
+    public void getFirstThrowsExceptionWhenEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertThrows(NoSuchElementException.class,
+                LinkedListQueue::getFirst);}
+    @Test
+    public void getFirstReturnTheSameValueAfterAddFirst(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        int elementToAdd = 4;
+        LinkedListQueue.addFirst(elementToAdd);
+        assertEquals(elementToAdd, LinkedListQueue.getFirst());
+    }
+    @Test
+    public void getFirstIsNotChangedAfterAddLast() {
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(15);
+        LinkedListQueue.addLast(25);
+        assertEquals(1, LinkedListQueue.getFirst());
+    }
+    /**
+     * Tests for getLast()
+     */
+    @Test
+    public void getLastThrowsExceptionWhenEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertThrows(NoSuchElementException.class,
+                LinkedListQueue::getLast);
+    }
+    @Test
+    public void getLastReturnTheSameValueAfterAddLast(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        int elementToAdd = 4;
+        LinkedListQueue.addLast(elementToAdd);
+        assertEquals(elementToAdd, LinkedListQueue.getLast());
+    }
+    @Test
+    public void getLastIsNotChangedAfterAddFirst(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addFirst(1);
+        LinkedListQueue.addFirst(15);
+        LinkedListQueue.addFirst(25);
+        assertEquals(1, LinkedListQueue.getLast());}
+    /**
+     * Tests for get()
+     */
+    @Test
+    public void getThrowsExceptionWhenEmpty(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        assertThrows(NoSuchElementException.class,
+                () -> LinkedListQueue.get(0));
+    }
+    @Test
+    public void getThrowsExceptionWhenIndexIncorrect(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        assertThrows(IndexOutOfBoundsException.class,
+                () -> LinkedListQueue.get(-1));
+    }
+    @Test
+    public void getIsChangedAfterAddFirst(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(10);
+        LinkedListQueue.addLast(15);
+        LinkedListQueue.addLast(20);
+        LinkedListQueue.addLast(25);
+        int getBeforeAdding = LinkedListQueue.get(3);
+        LinkedListQueue.addFirst(15);
+        assertNotEquals(getBeforeAdding, LinkedListQueue.get(3));
+        assertEquals(getBeforeAdding, LinkedListQueue.get(4));
+    }
+    @Test
+    public void getIsNotChangedAfterAddLast(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<Integer>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(10);
+        int getBeforeAdding = LinkedListQueue.get(1);
+        LinkedListQueue.addLast(15);
+        assertEquals(getBeforeAdding, LinkedListQueue.get(1));
+    }
+    /**
+     * Tests for clear()
+     */
+    @Test
+    public void isEmptyRetrunsTrueAfterClearMethod(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        assertTrue(LinkedListQueue.isEmpty());
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.addLast(10);
+        LinkedListQueue.addLast(15);
+        assertFalse(LinkedListQueue.isEmpty());
+        assertEquals(3, LinkedListQueue.size());
+        LinkedListQueue.clear();
+        assertTrue(LinkedListQueue.isEmpty());
+        assertEquals(0, LinkedListQueue.size());
+    }
+    /**
+     * Tests for isEmpty()
+     */
+    @Test
+    public void isEmptyRetrunsTrueForListWithoutAnyElements(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        assertTrue(LinkedListQueue.isEmpty());
+    }
+    @Test
+    public void isEmptyRetrunsFalseForListWithOneElement(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        LinkedListQueue.addFirst(1);
+        assertFalse(LinkedListQueue.isEmpty());
+    }
+    @Test
+    public void isEmptyRetrunsFalseForListWithMultipleElements(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        LinkedListQueue.addFirst(1);
+        LinkedListQueue.addFirst(10);
+        assertFalse(LinkedListQueue.isEmpty());
+    }
+    @Test
+    public void isEmptyRetrunsTrueAfterAddLastAndRemoveFirst(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        LinkedListQueue.addLast(1);
+        LinkedListQueue.removeFirst();
+        assertTrue(LinkedListQueue.isEmpty());
+    }
+    @Test
+    public void isEmptyRetrunsTrueAfterAddFirstAndRemoveLast(){
+        LinkedListQueue<Integer> LinkedListQueue = new LinkedListQueue<>();
+        LinkedListQueue.addFirst(1);
+        LinkedListQueue.removeLast();
+        assertTrue(LinkedListQueue.isEmpty());
+    }
+
+    @Test
+    public void offerAddsElementToTheEndOfFIFOQueue(){
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        assertTrue(linkedListQueue.isEmpty());
+
+        linkedListQueue.offer(1);
+        linkedListQueue.offer(4);
+        assertEquals(linkedListQueue.getFirst(), 1);
+        assertEquals(linkedListQueue.getLast(), 4);
+
+        linkedListQueue.offer(2);
+        assertEquals(linkedListQueue.getFirst(), 1);
+        assertEquals(linkedListQueue.getLast(), 2);
+    }
+
+    @Test
+    public void addAddsElementToTheEndOfFIFOQueue(){
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        assertTrue(linkedListQueue.isEmpty());
+
+        linkedListQueue.offer(1);
+        linkedListQueue.offer(4);
+        assertEquals(linkedListQueue.getFirst(), 1);
+        assertEquals(linkedListQueue.getLast(), 4);
+
+        linkedListQueue.offer(2);
+        assertEquals(linkedListQueue.getFirst(), 1);
+        assertEquals(linkedListQueue.getLast(), 2);
+    }
+
+    @Test
+    public void pollRemovesElementFromTheBeginningOfFIFOQueue(){
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        assertTrue(linkedListQueue.isEmpty());
+
+        linkedListQueue.offer(1);
+        linkedListQueue.offer(4);
+        linkedListQueue.offer(2);
+        linkedListQueue.poll();
+        assertEquals(linkedListQueue.getFirst(), 4);
+        assertEquals(linkedListQueue.getLast(), 2);
+    }
+
+    @Test
+    public void removeRemovesElementFromTheBeginningOfFIFOQueue(){
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        assertTrue(linkedListQueue.isEmpty());
+
+        linkedListQueue.offer(1);
+        linkedListQueue.offer(4);
+        linkedListQueue.offer(2);
+        linkedListQueue.poll();
+        assertEquals(linkedListQueue.getFirst(), 4);
+        assertEquals(linkedListQueue.getLast(), 2);
+    }
+
+    static Stream<Arguments> LinkedListQueueData(){
+        return  Stream.of(
+                arguments((Object)new Integer[]{1,2,3,4})
+                //arguments(3, null, 4),
+                //arguments("One", "Two", "Three"),
+                //arguments(5.0, 4.4, 3.9)
+        );
+    }
 }
+
